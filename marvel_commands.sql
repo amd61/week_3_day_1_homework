dedfd87182f903cb0418839b7be55d39387ada60
amd61 <arlenem.mcdaid@googlemail.com>
2017-01-23 23:00:51 +0000

    commands homework

---
+++ marvel_commands.sql
@@ -0,0 +1,11 @@
+SELECT * FROM movies;
+SELECT name FROM people;
+UPDATE people SET name = 'Alastair Hood' WHERE name = 'Allisder Hood';
+SELECT name FROM people WHERE name = 'Arlene McDaid';
+DELETE FROM movies WHERE title = 'Batman Begins';
+INSERT INTO people (name) VALUES ('Alan Russell');
+DELETE FROM people WHERE name = 'Harvey Wheaton';
+INSERT INTO movies (title, show_time) VALUES ('Guardians of the Galaxy 2','24:00');
+UPDATE movies SET show_time = '21:30' WHERE title = 'Guardians of the Galaxy';
+
+
